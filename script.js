1028c95088aaba16f7bcf0af11e6a6e65ba3de25
Abdul Ahmed <abdulubdaa@gmail.com>
2021-05-27 22:18:11 +0100

    found new way to add breaks and trying to integrate

--- script.js
+++ script.js
@@ -11,17 +11,22 @@ if(questions[i].checked == true){
     currentQuestion = questions[i].value;
 
     if(currentQuestion == "YFC"){
-        currentQuestion = "Because I like doing hard things and coding is allegedly hard" + "<br>" + "Because I heard good things from an F&C alumni" + "<br>" + "Because I want to ascend";
+        currentQuestion = "Because I like doing hard things and coding is allegedly hard" + "<br>" + 
+        "Because I heard good things from an F&C alumni" + "<br>" + 
+        "Because I want to ascend";
     }
     
     else if(currentQuestion == "YAB"){
-        currentQuestion = "He's meticulous and he knows French (kinda)" + "\n" + 
-        "Years competing (and winning) in sports forged commitment to excellence" + "\n" +
+        currentQuestion = "He's meticulous and he knows French (kinda)" + "<br>" + 
+        "Years competing (and winning) in sports forged commitment to excellence" + "<br>" +
         "He wants to ascend";
     }
     
     else if(currentQuestion == "TYW"){
-        currentQuestion =  "If you have as much fun viewing these as I did completing them then we're perfect for each other";
+        currentQuestion =  "If you have as much fun viewing these as I did completing them then we're perfect for each other" + "<br>" +
+        "<ul><li><a href=https://www.freecodecamp.org/fcc5c365cd4-a634-4737-9abd-7a469b0c75b0> Abdul's excellent freeCodeCamp adventure </a></li>" + "<br>" + 
+        "<li><a href=https://www.codewars.com/users/09ahmeaa> Abdul's battles in the Codewars </a> </li>" + "<br>" + 
+        "<li><a href=""> Abdul's GitHub repository </a> </li> </ul>";
         }
         
     document.getElementById("output").innerHTML = currentQuestion;
