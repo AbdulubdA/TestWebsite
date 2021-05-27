6d22a766ccf31e6b0ffd548e4795fe1496f058c4
Abdul Ahmed <abdulubdaa@gmail.com>
2021-05-27 20:06:42 +0100

    blah

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var outputToBox = 0;
 
-function ProduceAnswer(){
+function produceAnswer(){
 
 var questions = document.getElementsByName("qp");
 var currentQuestion = questions[0].value;
@@ -9,6 +9,7 @@ for(var i = 0; i < questions.length; i++){
 if(questions[i].checked == true){
 
     currentQuestion = questions[i].value;
+    
 }
 }
 
@@ -20,8 +21,8 @@ if(currentQuestion === "YFC"){
     <li>Because I want to ascend</li>
   </ul>;
 }
-else if(currentQuestion === "YAB"){
 
+else if(currentQuestion === "YAB"){
 outputToBox = <ul>
 <li>He's meticulous and he knows French (kinda)</li>
 <li>Years competing (and winning) in sports forged commitment to excellence</li>
@@ -30,9 +31,7 @@ outputToBox = <ul>
 }
 
 else if(currentQuestion === "TYW"){
-
     outputToBox =  "If you have as much fun viewing these as I did completing them then we're perfect for each other" <br>
-
     <ul>
       <li><a href="https://www.freecodecamp.org/fcc5c365cd4-a634-4737-9abd-7a469b0c75b0"> Abdul's excellent freeCodeCamp adventure </a></li>
       <li><a href="https://www.codewars.com/users/09ahmeaa"> Abdul's battles in the Codewars </a> </li>
@@ -40,14 +39,7 @@ else if(currentQuestion === "TYW"){
     </ul>;
     }
 
-    printOutput();
-
+    document.getElementById("output").innerHTML = outputToBox;
 }
 
-function printOutput(){
 
-    document.getElementById("output").textContent = outputToBox;
-
-}
-
-printOutput()
