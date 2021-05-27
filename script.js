026dc45b233271abf097d17b424ec310a50d6573
Abdul Ahmed <abdulubdaa@gmail.com>
2021-05-27 20:07:23 +0100

    blah

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if(questions[i].checked == true){
 }
 }
 
-if(currentQuestion === "YFC"){
+if(currentQuestion == "YFC"){
     outputToBox = 
     <ul>
     <li>Because I like doing hard things and coding is allegedly hard</li>
@@ -22,7 +22,7 @@ if(currentQuestion === "YFC"){
   </ul>;
 }
 
-else if(currentQuestion === "YAB"){
+else if(currentQuestion == "YAB"){
 outputToBox = <ul>
 <li>He's meticulous and he knows French (kinda)</li>
 <li>Years competing (and winning) in sports forged commitment to excellence</li>
@@ -30,7 +30,7 @@ outputToBox = <ul>
 </ul>;
 }
 
-else if(currentQuestion === "TYW"){
+else if(currentQuestion == "TYW"){
     outputToBox =  "If you have as much fun viewing these as I did completing them then we're perfect for each other" <br>
     <ul>
       <li><a href="https://www.freecodecamp.org/fcc5c365cd4-a634-4737-9abd-7a469b0c75b0"> Abdul's excellent freeCodeCamp adventure </a></li>
