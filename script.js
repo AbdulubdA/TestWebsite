c973a7dd65a6fee28aee1daf486856875e5be7cc
Abdul Ahmed <abdulubdaa@gmail.com>
2021-06-09 17:22:31 +0100

    added js stuff about slideshow

--- script.js
+++ script.js
@@ -53,4 +53,35 @@ document.getElementById(tabNo).style.display = "block"; /*shows current tab and 
 evt.currentTarget.className += " active";
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
+/* next and previous controls*/
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+
+if (n > slides.length){slideIndex = 1} /* makes clicking next arrow cyclical*/
+if (n < 1){slideIndex = slides.length}
+
+for(var i = 0; i < slides.length; i++){
+slides[i].style.display = "none"; /* makes every slide inactive at first*/
+}
+for(var i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += "active";
+
+
+}
