c3e9d9904f83038a46ffd8bfae4f3e6f39009e75
Abdul Ahmed <abdulubdaa@gmail.com>
2021-06-10 16:00:14 +0100

    found how to make dots right

--- script.js
+++ script.js
@@ -65,10 +65,10 @@ function plusSlides(n) {
     showSlides(slideIndex = n);
   }
 
-function showSlides(n){
-var i;
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
 
 if (n > slides.length){slideIndex = 1} /* makes clicking next arrow cyclical*/
 if (n < 1){slideIndex = slides.length}
@@ -76,40 +76,11 @@ if (n < 1){slideIndex = slides.length}
 for(i = 0; i < slides.length; i++){
 slides[i].style.display = "none"; /* makes every slide inactive at first*/
 }
-for(i = 0; i < dots.length; i++){
+for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
 }
 
 slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += "active";
+  dots[slideIndex-1].className += "active";
 
 }
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}*/
