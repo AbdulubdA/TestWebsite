db49f7ab0075868eee1f2bd0091c1a13605b343b
Abdul Ahmed <abdulubdaa@gmail.com>
2021-06-28 19:04:25 +0100

    blah

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ a.appendChild(link1);
 a.title = "Abdul's excellent freeCodeCamp adventure";
 // Set the href property.
 a.href = "https://www.freecodecamp.org/fcc5c365cd4-a634-4737-9abd-7a469b0c75b0";
-// Append the anchor element to the body.
-document.output.appendChild(a);
+
 
 var b = document.createElement('b');
 var link2 = document.createTextNode("Abdul's battles in the Codewars");
@@ -52,7 +51,8 @@ if(questions[i].checked == true){
     }
     
     else if(currentQuestion == "TYW"){
-        currentQuestion = "";
+        currentQuestion = // Append the anchor element to the body.
+        document.output.appendChild(a);
         
         }
         
